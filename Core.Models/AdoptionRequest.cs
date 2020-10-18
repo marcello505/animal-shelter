3c9233378ccc8b5354dc41b98423941056b104e6
marcello505 <32366042+marcello505@users.noreply.github.com>
2020-10-18 03:31:55 +0200

    Create AdoptionRequest.cs

---
+++ Core.Models/AdoptionRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Core.Models
+{
+    public class AdoptionRequest
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Telephone { get; set; }
+        [ForeignKey("Animal")]
+        [Required]
+        public int AnimalId1 { get; set; }
+        [ForeignKey("Animal")]
+        public int? AnimalId2 { get; set; }
+        [ForeignKey("Animal")]
+        public int? AnimalId3 { get; set; }
+
+    }
+}
