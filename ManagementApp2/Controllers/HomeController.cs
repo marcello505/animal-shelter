3ae3019e7d61f0022e13688bf411c27d70243225
marcello505 <32366042+marcello505@users.noreply.github.com>
2020-10-19 00:10:24 +0200

    Added Standard Identity Implementation.

---
+++ ManagementApp2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ManagementApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using ManagementApplication.Data;
+
+namespace ManagementApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+
+            IdentitySeeder.EnsurePopulated(_userManager).Wait();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ManagementApp2/Models/TreatmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApplication.Models
+{
+    public class TreatmentViewModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public decimal? Cost { get; set; }
+        public int? MinimumAgeInMonths { get; set; }
+        public string TreatmentDoneBy { get; set; }
+        public DateTime DateOfTreatment { get; set; }
+        public int? AnimalId { get; set; }
+        public string Animal { get; set; }
+    }
+}
