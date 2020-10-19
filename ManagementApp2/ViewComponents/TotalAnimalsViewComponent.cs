aa8f9f0ca2b1250629c7ed4f6b430ac63ad8d572
marcello505 <32366042+marcello505@users.noreply.github.com>
2020-10-19 03:57:40 +0200

    Added Simple ViewComponent to show total amount of animals at all times.

---
+++ ManagementApp2/ViewComponents/TotalAnimalsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.DomainServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementApplication.ViewComponents
+{
+    public class TotalAnimalsViewComponent : ViewComponent
+    {
+        private readonly IAnimalRepository _context;
+        public TotalAnimalsViewComponent(IAnimalRepository context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var itemCount = await Task.Run(_context.GetAll().Count);
+            return View(itemCount);
+        }
+    }
+}
