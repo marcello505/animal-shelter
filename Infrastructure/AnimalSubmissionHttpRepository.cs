8c31cb1f9d9a0bad9b7f75a72f1cc7846cb34712
marcello505 <32366042+marcello505@users.noreply.github.com>
2020-10-19 18:46:19 +0200

    Setup basic HttpRepository

---
+++ Infrastructure/AnimalSubmissionHttpRepository.cs
@@ -0,0 +1,57 @@
+﻿using Core.DomainServices;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class AnimalSubmissionHttpRepository : IAnimalSubmissionRepository
+    {
+        private HttpClient _httpClient;
+        public AnimalSubmissionHttpRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task Add(AnimalSubmission animalSubmission)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnimalSubmission Get(int id)
+        {
+            AnimalSubmission result = null;
+            var response = _httpClient.GetAsync("api/v2/animalsubmissions/" + id).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                result = JsonSerializer.Deserialize<AnimalSubmission>(json);
+                
+            }
+            return result;
+        }
+
+        public IEnumerable<AnimalSubmission> GetAll()
+        {
+            IEnumerable<AnimalSubmission> result = null;
+            var response = _httpClient.GetAsync("api/v2/animalsubmissions").GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                result = JsonSerializer.Deserialize<IEnumerable<AnimalSubmission>>(json);
+            }
+            return result;
+        }
+
+        public Task Update(AnimalSubmission animalSubmission)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
