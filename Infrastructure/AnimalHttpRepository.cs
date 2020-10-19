f6245ee26d54ade446494324171393b355f7108e
marcello505 <32366042+marcello505@users.noreply.github.com>
2020-10-19 21:26:27 +0200

    Create AnimalHttpRepository.cs

---
+++ Infrastructure/AnimalHttpRepository.cs
@@ -0,0 +1,69 @@
+﻿using Core.DomainServices;
+using Core.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class AnimalHttpRepository : IAnimalRepository
+    {
+        private HttpClient _httpClient;
+        public AnimalHttpRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task Add(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Animal Get(int id)
+        {
+            Animal result = null;
+            var response = _httpClient.GetAsync("api/v2/animals/" + id).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                result = JsonConvert.DeserializeObject<Animal>(json);
+            }
+            return result;
+        }
+
+        public IEnumerable<Animal> GetAll()
+        {
+            IEnumerable<Animal> result = null;
+            var response = _httpClient.GetAsync("api/v2/animals").GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                result = JsonConvert.DeserializeObject<IEnumerable<Animal>>(json);
+            }
+            return result;
+        }
+
+        public IEnumerable<Animal> GetByCageId(int cageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Animal animal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
